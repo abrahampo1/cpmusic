e39f886588c247e5dc4a09dadd7a206dcb893bb7
Abraham <53398615+abrahampo1@users.noreply.github.com>
2021-06-08 19:03:43 +0200

    Update display.php

--- display.php
+++ display.php
@@ -515,8 +515,7 @@ $volumen = $volumen["value"];
 <div id="myModal" class="modal">
 
     <!-- Modal content -->
-    <div class="modal-content" id="noticia">
-    </div>
+    <div class="modal-content" id="noticia"></div>
 
 </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
